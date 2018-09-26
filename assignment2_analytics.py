8216134678827653603a80636930d200e1ca7608
Joey Braspenning <braspenning@student29.strw.leidenuniv.nl>
2018-09-26 16:57:18 +0200

    kleine veranderingen aan alle files zodat ze beter werken

--- assignment2_analytics.py
+++ assignment2_analytics.py
@@ -21,22 +21,26 @@ def test_buckets():
     np.savetxt("results.csv", results)
 
 def test_accuracy():
-    max_bucket = 11
-    min_bucket = 10
+    max_bucket = 15
+    min_bucket = 5
     num_buckets = max_bucket-min_bucket
-    size = np.logspace(3,7,base=10,num=4, dtype=np.int32)
+    size = [10**3, 10**4, 10**5, 10**6]
     results = np.empty([len(size)*num_buckets,4])
     current_step=0
     for num in range(len(size)):
         for buckets in range(min_bucket,max_bucket):
             temp = np.empty(10)
+            acc = np.empty(10)
             size_step = size[num%4]
             for j in range(10):
-                temp[j] = (a2.estimate_cardinality([random.randint(0, 2**32-1) for i in range(size_step)],buckets))
+                s = [random.randint(0, 2**32-1) for i in range(size_step)]
+                temp[j] = (a2.estimate_cardinality(s,buckets))
+                u = np.unique(s)
+                acc[j] = u/temp[j]
             results[current_step,0] = size_step
             results[current_step,1] = buckets
-            results[current_step,2] = size_step/np.mean(temp)
-            results[current_step,3] = np.std(size_step/temp)
+            results[current_step,2] = np.mean(acc)
+            results[current_step,3] = np.std(acc)
             current_step += 1
             
 
--- assignment2_plotting.py
+++ assignment2_plotting.py
@@ -20,3 +20,14 @@ def bucket_plots():
             axes[1,num_buck-5].set_title("bucket size {}".format(buckets))
     plt.show()
 
+def accuracy_plots():
+    ar = np.loadtxt("results_accuracy.csv")
+    fig, axes  = plt.subplots(nrows=1,ncols=4)
+    accuracy = np.abs(ar[:,2] - 1)
+    size = [10**3, 10**4, 10**5, 10**6]
+    for i in range(4):
+        axes[i].scatter(ar[ar[:,0]==size[i],1],np.abs(ar[ar[:,0]==size[i],2]-1)*100)
+        axes[i].errorbar(ar[ar[:,0]==size[i],1],np.abs(ar[ar[:,0]==size[i],2]-1)*100, yerr=ar[ar[:,0]==size[i],3], linestyle='None')
+        axes[i].set_title("{}".format(size[i]))
+    plt.show()
+accuracy_plots()
