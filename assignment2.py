03657b3277e48eb501191b8f027350347397e25f
Joey Braspenning <braspenning@student29.strw.leidenuniv.nl>
2018-09-27 14:08:29 +0200

    implemented the Flajolet Martin algorithm and ran some tests

--- assignment2.py
+++ assignment2.py
@@ -22,8 +22,8 @@ def trailing_zeroes(num):
   return p
 
 #@timing
-def estimate_cardinality(values, k):
-  """Estimates the number of unique elements in the input set values.
+def estimate_cardinality_loglog(values, k):
+  """Estimates the number of unique elements in the input set values using the LogLog algorithm
 
   Arguments:
     values: An iterator of hashable elements to estimate the cardinality of.
@@ -37,6 +37,69 @@ def estimate_cardinality(values, k):
     bucket_hash = h >> k
     max_zeroes[bucket] = max(max_zeroes[bucket], trailing_zeroes(bucket_hash))
   return 2 ** (float(sum(max_zeroes)) / num_buckets) * num_buckets * 0.79402
+
+
+
+def estimate_cardinality_FM(values):
+  """Estimates the number of unique elements in the input set values using the LogLog algortihm
+
+  Arguments:
+    values: An iterator of hashable elements to estimate the cardinality of.
+  """
+  max_zeroes = 0
+  for value in values: 
+    h = value
+    max_zeroes = max(max_zeroes, trailing_zeroes(h))
+  return 2 ** max_zeroes
+
+
+def estimate_cardinality_FM_split(values, split_number):
+  """Estimates the number of unique elements in the input set values using the LogLog algortihm
+
+  Arguments:
+    values: An iterator of hashable elements to estimate the cardinality of.
+  """
+  max_zeroes = np.zeros(split_number)
+  value_split = np.zeros(split_number)
+  value_split = np.split(np.array(values), split_number)
+  for j in range(split_number):
+     max_zeroes_split = 0
+     for value in list(value_split[j]): 
+        h = value
+        max_zeroes_split = max(max_zeroes_split, trailing_zeroes(h))
+     max_zeroes[j] = max_zeroes_split   
+  return 2 ** np.mean(max_zeroes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- assignment2_analytics.py
+++ assignment2_analytics.py
@@ -2,48 +2,72 @@ import numpy as np
 import random
 import assignment2 as a2
 
-def test_buckets():
-    max_bucket = 15
-    min_bucket = 5
-    num_buckets = max_bucket-min_bucket
-    size = np.logspace(3,7,base=10,num=100, dtype=np.int32)
-    results = np.empty([len(size)*num_buckets,3])
-    current_step=0
-    for num in range(len(size)):
-        print(num)
-        for buckets in range(min_bucket,max_bucket):
-            size_step = size[num%100]
-            results[current_step,0] = size_step
-            results[current_step,1] = buckets
-            results[current_step,2] = (a2.estimate_cardinality([random.randint(0, 2**32-1) for i in range(size_step)],buckets))
-            current_step += 1
+#def test_buckets():
+#    max_bucket = 15
+#    min_bucket = 5
+#    num_buckets = max_bucket-min_bucket
+#    size = np.logspace(3,7,base=10,num=100, dtype=np.int32)
+#    results = np.empty([len(size)*num_buckets,3])
+#    current_step=0
+#    for num in range(len(size)):
+#        print(num)
+#        for buckets in range(min_bucket,max_bucket):
+#            size_step = size[num%100]
+#            results[current_step,0] = size_step
+#            results[current_step,1] = buckets
+#            results[current_step,2] = (a2.estimate_cardinality([random.randint(0, 2**32-1) for i in range(size_step)],buckets))
+#            current_step += 1
+#
+#    np.savetxt("results.csv", results)
+#
+#def test_accuracy():
+#    max_bucket = 15
+#    min_bucket = 5
+#    num_buckets = max_bucket-min_bucket
+#    size = [10**3, 10**4, 10**5, 10**6]
+#    results = np.empty([len(size)*num_buckets,4])
+#    current_step=0
+#    for num in range(len(size)):
+#        for buckets in range(min_bucket,max_bucket):
+#            temp = np.empty(10)
+#            acc = np.empty(10)
+#            size_step = size[num%4]
+#            for j in range(10):
+#                s = [random.randint(0, 2**32-1) for i in range(size_step)]
+#                temp[j] = (a2.estimate_cardinality(s,buckets))
+#                u = np.unique(s)
+#                acc[j] = u/temp[j]
+#            results[current_step,0] = size_step
+#            results[current_step,1] = buckets
+#            results[current_step,2] = np.mean(acc)
+#            results[current_step,3] = np.std(acc)
+#            current_step += 1
+#            
+#
+#    np.savetxt("results_accuracy.csv", results)
 
-    np.savetxt("results.csv", results)
+def test_FM():
+    '''
+    Test the FM_split function by running it num_iter times and taking the median of the outcome
+    Splits = the number of groups the hashfunction is partitioned in
+    We generate a random number sequence with numbers of lengt 32bits and different sequence lengths proportional to the size_list
+    The random sequence is probed for unique elements and the first "size" unique elements are fed into the cardinality estimator
+    '''
+    size_list = [10**3, 10**4, 10**5, 10**6]
+    e = np.zeros(len(size_list))
+    num_iter = 10
+    splits = 10
+    for k in range(len(size_list)):
+        print(k)
+        size = size_list[k]
+        e_t = []
+        for j in range(num_iter):
+            s = [random.randint(0, 2**32-1) for i in range(int(1.2*size))]
+            u = np.unique(s)
+            ss = u[:size]
+            e_t.append(a2.estimate_cardinality_FM_split(ss,splits))
+        e[k] = np.median(e_t)    
+    return e
+estimate = test_FM()
+print(estimate)
 
-def test_accuracy():
-    max_bucket = 15
-    min_bucket = 5
-    num_buckets = max_bucket-min_bucket
-    size = [10**3, 10**4, 10**5, 10**6]
-    results = np.empty([len(size)*num_buckets,4])
-    current_step=0
-    for num in range(len(size)):
-        for buckets in range(min_bucket,max_bucket):
-            temp = np.empty(10)
-            acc = np.empty(10)
-            size_step = size[num%4]
-            for j in range(10):
-                s = [random.randint(0, 2**32-1) for i in range(size_step)]
-                temp[j] = (a2.estimate_cardinality(s,buckets))
-                u = np.unique(s)
-                acc[j] = u/temp[j]
-            results[current_step,0] = size_step
-            results[current_step,1] = buckets
-            results[current_step,2] = np.mean(acc)
-            results[current_step,3] = np.std(acc)
-            current_step += 1
-            
-
-    np.savetxt("results_accuracy.csv", results)
-
-test_accuracy()
