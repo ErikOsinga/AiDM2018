be0ec43f699e61e28ca565c834620ec07a321c81
Joeybraspenning <43174510+Joeybraspenning@users.noreply.github.com>
2018-10-11 17:25:14 +0200

    Assignment 3 doet nu 220sec over 50 signatures

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -2,8 +2,8 @@ import sys
 import numpy as np
 from scipy.sparse import coo_matrix
 
-pairs = np.load('./user_movie.npy')
 
+pairs = np.load('./user_movie.npy')
 print ('Setting the random seed to:', sys.argv[1])
 np.random.seed(int(sys.argv[1]))
 
@@ -19,16 +19,31 @@ def create_sparse_matrix2():
 
 X = create_sparse_matrix2()
 
+def find_signatures(M, X, cur_n):   
+   M[:,cur_n] = [X.indices[X.indptr[j]:X.indptr[j+1]][0] for j in range(X.shape[0])]
+   return M
+
+
 def create_signatures(X, num_sig):
 	# signature matrix
-	M = np.empty((X.shape[0],num_sig)) 
+	M = np.zeros((X.shape[0],num_sig)) 
+	M.fill(np.nan)
+	t_movies = movies
+	length = len(t_movies)
+	s = X.shape[0]
 	for i in range(num_sig): # create num_sig permutations 
-		np.random.shuffle(movies) # permute the movies
+		print(i)
+		#np.random.shuffle(movies) # permute the movies
+		#alternative to shuffle:
+		t_movies = np.mod(t_movies + np.random.randint(s, size=length),s)
 		# keep in mind, maybe permuting the matrix is quicker than creating a new
-		X = coo_matrix(X.data, (users, movies))
+		X = coo_matrix(X.data, (users, t_movies))
 		# save the indices of the first nonzero of every user for this permutation
-		M[:,i] = X.argmax(axis=1) # have to find a better way to find first nonzero
+		#M[:,i] = X.argmax(axis=1) # have to find a better way to find first nonzero\
+		
+		#use find_sigantures function
+		M = find_signatures(M, X.tocsr(), i)
 
 	return M
-
-# M = create_signatures(X, 100)
+M = create_signatures(X, 50)
+    
